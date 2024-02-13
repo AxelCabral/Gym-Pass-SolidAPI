05d4ee11e726344e07d4693cf0cf0fcd00428ebe
Axel Cabral <axel.cabral0102@gmail.com>
2024-02-12 21:36:51 -0300

    Updt 1.0

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,6 @@
 import fastify from 'fastify'
-import { PrismaClient } from '@prisma/client'
+import { appRoutes } from './http/routes'
 
 export const app = fastify()
 
-const prisma = new PrismaClient()
-
-prisma.user.create({
-  data: {
-    name: 'Axel Cabral',
-    email: 'axel.cabral0102@gmail.com',
-  },
-})
+app.register(appRoutes)
