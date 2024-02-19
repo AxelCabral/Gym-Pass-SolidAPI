6544e1d963f7b07dfa0f1df22baba102f554a182
Axel Cabral <axel.cabral0102@gmail.com>
2024-02-19 02:19:57 -0300

    Updt 1.9
    - Criação dos repositórios do Prisma
    - Configurada funcionalidade de identificação de usuário com JWT
    - Atualizadas variáveis ambiente
    - Adicionada rota de perfil

--- src/use-cases/fetch-nearby-gyms.ts
+++ src/use-cases/fetch-nearby-gyms.ts
@@ -1,22 +1,22 @@
 import { Gym } from '@prisma/client'
 import { GymsRepository } from '@/repositories/gyms-repository'
 
-interface FetchByNearbyUseCaseRequest {
+interface FetchNearbyGymsUseCaseRequest {
   userLatitude: number
   userLongitude: number
 }
 
-interface FetchByNearbyUseCaseResponse {
+interface FetchNearbyGymsUseCaseResponse {
   gyms: Gym[]
 }
 
-export class FetchByNearbyUseCase {
+export class FetchNearbyGymsUseCase {
   constructor(private gymsRepository: GymsRepository) {}
 
   async execute({
     userLatitude,
     userLongitude,
-  }: FetchByNearbyUseCaseRequest): Promise<FetchByNearbyUseCaseResponse> {
+  }: FetchNearbyGymsUseCaseRequest): Promise<FetchNearbyGymsUseCaseResponse> {
     const gyms = await this.gymsRepository.findManyNearby({
       latitude: userLatitude,
       longitude: userLongitude,
