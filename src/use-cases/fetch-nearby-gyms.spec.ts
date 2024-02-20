3b62f511c1958cfbd02e3eec639c42827c610a5c
Axel Cabral <axel.cabral0102@gmail.com>
2024-02-19 23:04:09 -0300

    Updt 2.0
    - Criação de ambiente para teste E2E
    - Criação de middleware de autenticação

--- src/use-cases/fetch-nearby-gyms.spec.ts
+++ src/use-cases/fetch-nearby-gyms.spec.ts
@@ -1,14 +1,14 @@
 import { InMemoryGymsRepository } from '@/repositories/in-memory/in-memory-gyms-repository'
 import { expect, describe, it, beforeEach } from 'vitest'
-import { FetchByNearbyUseCase } from './fetch-nearby-gyms'
+import { FetchNearbyGymsUseCase } from './fetch-nearby-gyms'
 
 let gymsRepository: InMemoryGymsRepository
-let sut: FetchByNearbyUseCase
+let sut: FetchNearbyGymsUseCase
 
 describe('Fetch Nearby Gyms Use Case', () => {
   beforeEach(async () => {
     gymsRepository = new InMemoryGymsRepository()
-    sut = new FetchByNearbyUseCase(gymsRepository)
+    sut = new FetchNearbyGymsUseCase(gymsRepository)
   })
 
   it('should be able to fetch nearby gyms', async () => {
