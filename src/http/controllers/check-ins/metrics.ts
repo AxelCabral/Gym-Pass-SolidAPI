673130b4b19d84007616765b1d1de08eed5c5325
Axel Cabral <axel.cabral0102@gmail.com>
2024-02-21 01:45:47 -0300

    Updt 2.1
    - Criação de ambiente para testes e2e (Banco de dados temporários que se apagam após teste no Docker)
    - Correções de nomenclaturas nos arquivos make
    - Criação dos testes e2e para User
    - Criação dos controllers de User, Gym e Check-in
    - Separação dos arquivos de rotas e declaração das rotas no app
    - Configurações das rotas individuais dos controllers

---
+++ src/http/controllers/check-ins/metrics.ts
@@ -0,0 +1,12 @@
+import { FastifyRequest, FastifyReply } from 'fastify'
+import { makeGetUserMetricsUseCase } from '@/use-cases/factories/make-get-user-metrics-use-case'
+
+export async function metrics(request: FastifyRequest, reply: FastifyReply) {
+  const searchGymsUseCase = makeGetUserMetricsUseCase()
+
+  const { checkInsCount } = await searchGymsUseCase.execute({
+    userId: request.user.sub,
+  })
+
+  return reply.status(201).send(checkInsCount)
+}
